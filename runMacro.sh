6f2a124b8b8749a673e9ade8b371e31a4b04f8fe
telenz <>
2013-07-18 16:52:01 +0000

    some cosmeticical changes

--- runMacro.sh
+++ runMacro.sh
@@ -4,7 +4,9 @@ start=$(date +%s) ## get current time/date to $start
 
 # ----------------------------------------------------------------------------------------------------
 echo "Time needed: " $(date -d "1970-01-01 $thetime sec" +"%H:%M:%S") / $thetime "secs" ## prints out the time ( like: 00:01:29 / 89 secs )
- Run whole analysis for MC
+# ----------------------------------------------------------------------------------------------------
+# Run whole analysis for MC
+# ----------------------------------------------------------------------------------------------------
 root -b -q jetphoton_mc.C+"(10**9,1)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=3;/g' "$i"
 root -b -q jetphoton_mc.C+"(10**9,2)"
@@ -12,8 +14,10 @@ for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=2;/g' "$i"
 root -b -q jetphoton_mc.C+"(10**9,2)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=1;/g' "$i"
 root -b -q jetphoton_mc.C+"(10**9,2)"
+# ----------------------------------------------------------------------------------------------------
 
 # Run whole analysis for data
+# ----------------------------------------------------------------------------------------------------
 root -b -q jetphoton_data.C+"(10**9,1)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=3;/g' "$i"
 root -b -q jetphoton_data.C+"(10**9,2)"
@@ -22,13 +26,15 @@ root -b -q jetphoton_data.C+"(10**9,2)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=1;/g' "$i"
 root -b -q jetphoton_data.C+"(10**9,2)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=3;/g' "$i"
+# ----------------------------------------------------------------------------------------------------
 
 # Postprocess the output
+# ----------------------------------------------------------------------------------------------------
 root -b scripts << EOF
 .L DataMCComparison.C+
 plotDataMCComparisonFINAL()
 EOF
-
+# ----------------------------------------------------------------------------------------------------
 # Remove all unnecessary files
 rm *.C~
 rm CODE/*.C~
