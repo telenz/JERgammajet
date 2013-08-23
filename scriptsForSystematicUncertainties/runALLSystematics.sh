af6007dcc4e829c1b4f58f4f5369274c0cf001f8
Teresa Lenz <telenz@tcx101.naf.desy.de>
2013-08-23 10:08:41 +0200

    adding the evaluation of the central values for data and mc

--- scriptsForSystematicUncertainties/runALLSystematics.sh
+++ scriptsForSystematicUncertainties/runALLSystematics.sh
@@ -4,6 +4,21 @@ start=$(date +%s) ## get current time/date to $start
 # ----------------------------------------------------------------------------------------------------
 # Evaluate all systematic Uncertainties
 # ----------------------------------------------------------------------------------------------------
+
+cd ../
+source runMacro.sh
+if [ -d plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc]
+  then
+    mv plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc_SAVED
+fi
+cp -r plots_2012/PF_L1CHS/mc/root_files plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc
+
+if [ -d plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data]
+  then
+    mv plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data_SAVED
+fi
+cp -r plots_2012/PF_L1CHS/data/root_files plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data
+
 source runFlavorMacro.sh
 source runPUMacro.sh
 source runJECMacro.sh
