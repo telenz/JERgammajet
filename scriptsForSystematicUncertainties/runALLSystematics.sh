f236d0b40d70d4422ed4e0d529d425aad976929c
Teresa Lenz <telenz@tcx131.naf.desy.de>
2013-08-26 11:47:15 +0200

    to avoid error message

--- scriptsForSystematicUncertainties/runALLSystematics.sh
+++ scriptsForSystematicUncertainties/runALLSystematics.sh
@@ -7,13 +7,13 @@ start=$(date +%s) ## get current time/date to $start
 
 cd ../
 source runMacro.sh
-if [ -d plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc]
+if [ -d plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc ]
   then
     mv plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc_SAVED
 fi
 cp -r plots_2012/PF_L1CHS/mc/root_files plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc
 
-if [ -d plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data]
+if [ -d plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data ]
   then
     mv plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data_SAVED
 fi
