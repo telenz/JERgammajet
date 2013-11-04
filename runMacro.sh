45a2354545478d5c466a5c07baf2c851029efb42
Teresa <teresa.lenz@desy.de>
2013-11-04 15:16:26 +0100

    root -b changed to root -b -l

--- runMacro.sh
+++ runMacro.sh
@@ -7,30 +7,30 @@ echo "Time needed: " $(date -d "1970-01-01 $thetime sec" +"%H:%M:%S") / $thetime
 # ----------------------------------------------------------------------------------------------------
 # Run whole analysis for MC
 # ----------------------------------------------------------------------------------------------------
-root -b -q jetphoton_mc.C+"(10**9,1)"
+root -b -l -q jetphoton_mc.C+"(10**9,1)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=3;/g' "$i"
-root -b -q jetphoton_mc.C+"(10**9,2)"
+root -b -l -q jetphoton_mc.C+"(10**9,2)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=2;/g' "$i"
-root -b -q jetphoton_mc.C+"(10**9,2)"
+root -b -l -q jetphoton_mc.C+"(10**9,2)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=1;/g' "$i"
-root -b -q jetphoton_mc.C+"(10**9,2)"
+root -b -l -q jetphoton_mc.C+"(10**9,2)"
 # ----------------------------------------------------------------------------------------------------
 
 # Run whole analysis for data
 # ----------------------------------------------------------------------------------------------------
-root -b -q jetphoton_data.C+"(10**9,1)"
+root -b -l -q jetphoton_data.C+"(10**9,1)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=3;/g' "$i"
-root -b -q jetphoton_data.C+"(10**9,2)"
+root -b -l -q jetphoton_data.C+"(10**9,2)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=2;/g' "$i"
-root -b -q jetphoton_data.C+"(10**9,2)"
+root -b -l -q jetphoton_data.C+"(10**9,2)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=1;/g' "$i"
-root -b -q jetphoton_data.C+"(10**9,2)"
+root -b -l -q jetphoton_data.C+"(10**9,2)"
 for i in CODE/myDeclarations.h ; sed -i 's/detJER=.;/detJER=3;/g' "$i"
 # ----------------------------------------------------------------------------------------------------
 
 # Postprocess the output
 # ----------------------------------------------------------------------------------------------------
-root -b scripts << EOF
+root -b -l scripts << EOF
 .L DataMCComparison.C+
 plotDataMCComparisonFINAL()
 EOF
