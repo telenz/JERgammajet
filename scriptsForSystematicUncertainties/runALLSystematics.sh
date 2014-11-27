268829b074b423c6802db17477c905ba8c5061b6
Teresa Lenz <teresa.lenz@desy.de>
2014-11-27 16:05:05 +0100

    have nices output

--- scriptsForSystematicUncertainties/runALLSystematics.sh
+++ scriptsForSystematicUncertainties/runALLSystematics.sh
@@ -6,26 +6,41 @@ start=$(date +%s) ## get current time/date to $start
 # ----------------------------------------------------------------------------------------------------
 
 cd ../
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% run Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 source runMacro.sh
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 if [ -d plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc ]
   then
     mv plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc_SAVED
+    rm -rf plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc
 fi
 cp -r plots_2012/PF_L1CHS/mc/root_files plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_mc
 
 if [ -d plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data ]
   then
     mv plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data_SAVED
+    rm -rf plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data
 fi
 cp -r plots_2012/PF_L1CHS/data/root_files plots_2012/PF_L1CHS/systematicUncertainties/root_files_FINAL_data
 
 cd scriptsForSystematicUncertainties/
 
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Flavor Uncertainty Evaluation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 source runFlavorMacro.sh
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PU Uncertainty Evaluation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 source runPUMacro.sh
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% JEC Uncertainty Evaluation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 source runJECMacro.sh
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% QCD Uncertainty Evaluation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 source runQCDMacro.sh
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MC Uncertainty Evaluation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 source runMCMacro.sh
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 rm *.sh~
 # ----------------------------------------------------------------------------------------------------
 
